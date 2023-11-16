60127036c4411062eada00a2fa6ff8babd71aca5
Christopher-VA <cvonaltenstadt@gmail.com>
2023-11-16 22:14:23 +0800

    Adding local storage for text area when save button clicked

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var date = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
-  var calander = $('.container');
+  var calander = $('.container-lg');
   var currentDay = $('#currentDay');
-  var saveBtn = $('.saveBtn');
   var calanderHour = $('.time-block');
   var hourCurrent = dayjs().hour();
-  var eventText = localStorage.getItem("event");
   currentDay.text(date);
+ 
+  calander.on('click', '.saveBtn', function() {
+    var hour = $(this).parent().attr('id');
+    var note = $(this).parent().children().eq(1).val();
+    localStorage.setItem(hour, note);
 
-
-  for (let i = 0; i < saveBtn.length; i++) {
-    saveBtn[i].addEventListener('click', function() {
-      var input = $(".description");
-      console.log(i);
-      localStorage.setItem("event", eventText);
-    })
-}  
+  });
 
   for (let i = 0; i < calanderHour.length; i++) {
     var currentRow = $(calanderHour[i]);
-    var rowHour = currentRow.attr("id").split('hour-')[1];
+    var currentRowId = currentRow.attr("id");
+    var rowHour = currentRowId.split('hour-')[1];
     if (rowHour < hourCurrent){
       currentRow.addClass("past");
     }
@@ -31,6 +25,10 @@ $(function () {
     }
     else {
       currentRow.addClass("present")
+    }
+    var savedNote = localStorage.getItem(currentRowId);
+    if (savedNote) {
+      currentRow.children().eq(1).val(savedNote);
     }
   }
 });
