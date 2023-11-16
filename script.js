81d1f5de3c7ddd39352a611f7c5e9c17c9e2b76c
Christopher-VA <cvonaltenstadt@gmail.com>
2023-11-16 21:08:50 +0800

    Added colors based on time showing whats passed and whats yet to come

--- script.js
+++ script.js
@@ -5,46 +5,32 @@ $(function () {
   var date = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
   var calander = $('.container');
   var currentDay = $('#currentDay');
-  var saveBtn = document.getElementsByClassName('saveBtn'); // need to make this a Jquery
-  currentDay.text(date);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var saveBtn = $('.saveBtn');
+  var calanderHour = $('.time-block');
   var hourCurrent = dayjs().hour();
-  console.log(date);
-  console.log(hourCurrent);
-  console.log(calander.parent());
+  var eventText = localStorage.getItem("event");
+  currentDay.text(date);
+
 
   for (let i = 0; i < saveBtn.length; i++) {
     saveBtn[i].addEventListener('click', function() {
-      console.log('yay' + i);
       var input = $(".description");
-      console.log(input[i]);
-      // localStorage.setItem(".container", input.value);
-      // var storedValue = localStorage.getItem(".container");
+      console.log(i);
+      localStorage.setItem("event", eventText);
     })
 }  
-  
-  // saveBtn.addEventListener('click', localStorage)
 
-  var timeColorUpdate = function(){
-    
+  for (let i = 0; i < calanderHour.length; i++) {
+    var currentRow = $(calanderHour[i]);
+    var rowHour = currentRow.attr("id").split('hour-')[1];
+    if (rowHour < hourCurrent){
+      currentRow.addClass("past");
+    }
+    else if (rowHour > hourCurrent){
+      currentRow.addClass("future");
+    }
+    else {
+      currentRow.addClass("present")
+    }
   }
-
-  timeColorUpdate();
 });
